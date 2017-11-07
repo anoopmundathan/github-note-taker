04a1d30a5e49fa77c75659cbf89ca868547d1586
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-11-07 19:08:11 +0000

    Create First component

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import GitHubNoteTaker from './components/GitHubNoteTaker'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <GitHubNoteTaker />
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/GitHubNoteTaker.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const GitHubNoteTaker = () => <View><Text>GitHubNoteTaker</Text></View>
+export default GitHubNoteTaker
