d92913d113ba61d9557ef8857e6e21634e34aad3
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-11-10 04:49:41 +0000

    Add Picture component

---
+++ components/Picture.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View, Image, StyleSheet } from 'react-native'
+
+const Picture = (props) => {
+  return(
+    <View style={styles.picContainer}>
+      <Image 
+        style={styles.pic}
+        source={{uri: props.avatar}} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  picContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#5cab7d'
+  },
+  pic: {
+    width: 200,
+    height: 200,
+    borderRadius: 100
+  }
+})
+
+export default Picture
--- components/Profile.js
+++ components/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
+import Picture from './Picture'
 
 class Profile extends Component {
   render() {
@@ -14,11 +15,7 @@ class Profile extends Component {
 
     return(
       <View style={{flex: 1}}>
-        <View style={styles.picContainer}>
-          <Image 
-            style={styles.pic}
-            source={{uri: avatar_url}} />
-        </View>
+        <Picture avatar={avatar_url} />
         <View style={styles.infoContainer}>
           <View style={styles.info}>
             <Text>{company}</Text>
@@ -45,17 +42,6 @@ class Profile extends Component {
 }
 
 const styles = StyleSheet.create({
-  picContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#5cab7d'
-  },
-  pic: {
-    width: 200,
-    height: 200,
-    borderRadius: 100
-  },
   infoContainer: {
     flex: 1
   },
