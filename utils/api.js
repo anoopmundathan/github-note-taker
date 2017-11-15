d56c28b4d8f19e7f2c447723b7bad3131459caca
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-11-15 21:50:37 +0000

    Get repos

--- utils/api.js
+++ utils/api.js
@@ -3,6 +3,11 @@ const api = {
     username = username.toLowerCase().trim()
     const url =`https://api.github.com/users/${username}`
     return fetch(url).then(res => res.json())
+  },
+  getRepos(username) {
+    username = username.toLowerCase().trim()
+    const url =`https://api.github.com/users/${username}/repos`
+    return fetch(url).then(res => res.json())
   }
 }
 
