46d02d77a7661a7241fc6da9eb8b67a1bee1b2ce
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-11-16 05:30:54 +0000

    Add firebase REST Api to persist data

--- utils/api.js
+++ utils/api.js
@@ -8,6 +8,19 @@ const api = {
     username = username.toLowerCase().trim()
     const url =`https://api.github.com/users/${username}/repos`
     return fetch(url).then(res => res.json())
+  },
+  getNotes(username) {
+    username = username.toLowerCase().trim()
+    const url = `https://github-note-taker-7b6ca.firebaseio.com/${username}.json`
+    return fetch(url).then(res => res.json())
+  },
+  addNote(username, note) {
+    username = username.toLowerCase().trim()
+    const url = `https://github-note-taker-7b6ca.firebaseio.com/${username}.json`
+    return fetch(url, {
+      method: 'post',
+      body: JSON.stringify(note)
+    }).then(res => res.json())
   }
 }
 
